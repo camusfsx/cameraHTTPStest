9fcc998388b8c13b9524fe5adcd0f6ac1e612c3f
FengShixiang <f1992863292@outlook.com>
2018-08-20 09:52:24 +0800

    2018/8/20test

--- js/main.js
+++ js/main.js
@@ -1,1 +1,28 @@
+var front = true
+initCameraContext()
+document.getElementById('cameraFlip').onclick = () => {
+    front = !front
+    initCameraContext();
+}
+function initCameraContext(){
+    var cameraContext = navigator.mediaDevices.getUserMedia({
+        video: {
+            width: { min: 400, max: 1920 },
+            height: { min: 300, max: 1080 },
+            facingMode: (front ? "user" : "environment"),//or "environment" 此变量对surface暂时无效
+            frameRate: { ideal: 10, max: 24 }//ideal会影响到正反摄像头的选取
+        },
+        audio: true
+    }).then(function (stream) {
+        var video = document.getElementById('video')
+        var front = false
+        video.srcObject = stream
+        video.onloadedmetadata = function (e) {
+            console.log("AudioTracks", stream.getAudioTracks())
+            console.log("VideoTracks", stream.getVideoTracks())
+        }
+    }).catch(function (err) {
+        console.log('Rejected!', err)
+    })
+}
 
