ec84c64e173306390cda6da5f80d4215684cb8b9
FengShixiang <f1992863292@outlook.com>
2018-08-23 11:45:51 +0800

    otherfix

--- js/main.js
+++ js/main.js
@@ -8,16 +8,15 @@ document.getElementById('cameraFlip').onclick = () => {
 function initCameraContext(){
     var cameraContext = navigator.mediaDevices.getUserMedia({
         video: {
-            width: { min: 800, max: 1920 },
-            height: { min: 450, max: 1080 },
+            //width: { min: 800, max: 1920 },
+            //height: { min: 450, max: 1080 },
             facingMode: (front ? "user" : "environment"),//or "environment" 此变量对桌面端surface暂时无效,但对移动端设备有效
-            frameRate: { ideal: 10, max: 24 }//ideal会影响到正反摄像头的选取
+            //frameRate: { ideal: 10, max: 24 }//ideal会影响到正反摄像头的选取
         },
         audio: false
     }).then(function (stream) {
         var video = document.getElementById('video')
         video.srcObject = stream
-        video.play()
         console.dir(video.srcObject)
         video.onloadedmetadata = function (e) {
             console.log("AudioTracks", stream.getAudioTracks())
