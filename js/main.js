895b959dd85d180aa7bc088964737b90cd0f7a64
FengShixiang <f1992863292@outlook.com>
2018-08-23 11:28:22 +0800

    safarifix

--- js/main.js
+++ js/main.js
@@ -13,7 +13,7 @@ function initCameraContext(){
             facingMode: (front ? "user" : "environment"),//or "environment" 此变量对桌面端surface暂时无效,但对移动端设备有效
             frameRate: { ideal: 10, max: 24 }//ideal会影响到正反摄像头的选取
         },
-        audio: true
+        audio: false
     }).then(function (stream) {
         var video = document.getElementById('video')
         video.srcObject = stream
